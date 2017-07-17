54e722f7d9da254027f03c8efd936aab29fb0233
agorischek sterling <alex.gorischek@gmail.com>
2017-07-16 17:02:49 -0700

    Fix issue with array equality in initialization script

--- initialize.js
+++ initialize.js
@@ -1,6 +1,7 @@
-app.registers = app.default.registers;
+//Things to perform on page load after creating Vue instance
+app.registers = app.default.registers.slice();
 app.pointerA = app.default.pointerA;
 app.pointerB = app.default.pointerB;
 app.status = app.default.status;
-app.output = app.default.output;
+app.output = app.default.output.slice();
 app.currentCommandIndex = app.default.currentCommandIndex
